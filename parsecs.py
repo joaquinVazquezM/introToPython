6a4565575c4b11a2908f0270165f83b458a49e2d
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2024-08-20 12:22:11 -0600

    Ejercicio: calcular parsecs a lightyears

---
+++ parsecs.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+print("La fehca de hoy es: " + str(date.today()))
+
+parsecs = 11
+lightyears = parsecs * 3.26
+print(str(parsecs) + " parsecs is " + str(lightyears) + " lightyears")
