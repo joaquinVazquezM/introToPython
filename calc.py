9e527b2de9833208f265aa85b8b5157bac48bb78
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2024-08-20 11:41:32 -0600

    Ejercicio: Suma de dos numeros

---
+++ calc.py
@@ -0,0 +1,5 @@
+primer_numero = int(input("Escribe un número: "))
+segundo_numero = int(input("Escribe el segundo número: "))
+
+print("La suma de los números es_ ", primer_numero + segundo_numero)
+
