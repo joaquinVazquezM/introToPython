9bc19eb6e88636022db0f69aafe71597300b6d12
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2024-08-20 13:04:20 -0600

    Ejercicio: basura espacial (if-else)

---
+++ basuraEspacial.py
@@ -0,0 +1,6 @@
+object_size = int(input("Indica el tamaño del objeto: "))
+
+if object_size > 10:
+    print("Necesitamos vigilar el objeto")
+else:
+    print("El objeto no representa ninguna amenaza")
